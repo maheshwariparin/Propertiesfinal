32a84462cc494f6b1b6af1da0b7748145a6f29fb
maheshwariparin <125479941+maheshwariparin@users.noreply.github.com>
2025-04-20 18:56:29 +0530

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -57,6 +57,74 @@ function App() {
         <Route path="/plot" element={<Home />} />
         <Route path="/ourstory" element={<DMHProperties />} />
         <Route path="/rentclaculator" element={<Calculator />} />
+
+        <Route path="/3bhk-luxury-flat-shela" element={<Home />} />
+        <Route path="/2bhk-skyview-apartment-shela" element={<Home />} />
+        <Route path="/4bhk-bunglow-shela" element={<Home />} />
+        <Route path="/commercial-space-shela" element={<Home />} />
+        <Route path="/office-space-shela" element={<Home />} />
+
+        {/* Bopal */}
+        <Route path="/3bhk-flat-bopal" element={<Home />} />
+        <Route path="/2bhk-apartment-bopal" element={<Home />} />
+        <Route path="/4bhk-villa-bopal" element={<Home />} />
+        <Route path="/commercial-space-bopal" element={<Home />} />
+        <Route path="/office-space-bopal" element={<Home />} />
+
+        {/* South Bopal */}
+        <Route path="/3bhk-flat-south-bopal" element={<Home />} />
+        <Route path="/2bhk-apartment-south-bopal" element={<Home />} />
+        <Route path="/4bhk-bunglow-south-bopal" element={<Home />} />
+        <Route path="/commercial-space-south-bopal" element={<Home />} />
+        <Route path="/office-space-south-bopal" element={<Home />} />
+
+        {/* Jodhpur */}
+        <Route path="/3bhk-flat-jodhpur" element={<Home />} />
+        <Route path="/2bhk-apartment-jodhpur" element={<Home />} />
+        <Route path="/4bhk-bunglow-jodhpur" element={<Home />} />
+        <Route path="/commercial-space-jodhpur" element={<Home />} />
+        <Route path="/office-space-jodhpur" element={<Home />} />
+
+        {/* Anandnagar (lowercase + capital) */}
+        <Route path="/3bhk-flat-anandnagar" element={<Home />} />
+        <Route path="/2bhk-apartment-anandnagar" element={<Home />} />
+        <Route path="/4bhk-bunglow-anandnagar" element={<Home />} />
+        <Route path="/commercial-space-anandnagar" element={<Home />} />
+        <Route path="/office-space-anandnagar" element={<Home />} />
+        <Route path="/3bhk-flat-Anandnagar" element={<Home />} />
+        <Route path="/2bhk-apartment-Anandnagar" element={<Home />} />
+        <Route path="/4bhk-bunglow-Anandnagar" element={<Home />} />
+        <Route path="/commercial-space-Anandnagar" element={<Home />} />
+        <Route path="/office-space-Anandnagar" element={<Home />} />
+
+        {/* Ambli */}
+        <Route path="/3bhk-flat-Ambli" element={<Home />} />
+        <Route path="/2bhk-apartment-Ambli" element={<Home />} />
+        <Route path="/4bhk-bunglow-Ambli" element={<Home />} />
+        <Route path="/commercial-space-Ambli" element={<Home />} />
+        <Route path="/office-space-Ambli" element={<Home />} />
+
+        {/* Satellite */}
+        <Route path="/3bhk-flat-Satellite" element={<Home />} />
+        <Route path="/2bhk-apartment-Satellite" element={<Home />} />
+        <Route path="/4bhk-bunglow-Satellite" element={<Home />} />
+        <Route path="/commercial-space-Satellite" element={<Home />} />
+        <Route path="/office-space-Satellite" element={<Home />} />
+
+        {/* Ramdevnagar */}
+        <Route path="/3bhk-flat-Ramdevnagar" element={<Home />} />
+        <Route path="/2bhk-apartment-Ramdevnagar" element={<Home />} />
+        <Route path="/4bhk-bunglow-Ramdevnagar" element={<Home />} />
+        <Route path="/commercial-space-Ramdevnagar" element={<Home />} />
+        <Route path="/office-space-Ramdevnagar" element={<Home />} />
+
+
+        <Route path="/3bhk-flat-Ahmedabad" element={<Home />} />
+        <Route path="/2bhk-apartment-Ahmedabad" element={<Home />} />
+        <Route path="/4bhk-bunglow-Ahmedabad" element={<Home />} />
+        <Route path="/commercial-space-Ahmedabad" element={<Home />} />
+        <Route path="/office-space-Ahmedabad" element={<Home />} />
+
         </Routes>
     </Router>
   );
