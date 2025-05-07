4f18659957c9564108ae01f49ee8eae3bf39489c
parinmaheshwari <125479941+maheshwariparin@users.noreply.github.com>
2025-05-07 17:21:27 +0530

    Update ContactUs.jsx

--- src/component/ContactUs.jsx
+++ src/component/ContactUs.jsx
@@ -361,8 +361,7 @@ const ContactUs = () => {
       </div>
       
    <div className="w-full h-96 bg-gray-200 relative rounded-lg overflow-hidden shadow-xl">
-  <iframe
-    src="https://maps.app.goo.gl/ZE66ijyZJJCg7ByVA"
+ <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3672.2977302196555!2d72.47017819999999!3d23.012838000000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e9b7b562c2f8b%3A0x8030d632835a5a6!2sDMH%20Properties!5e0!3m2!1sen!2sin!4v1746618611237!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
     width="100%"
     height="100%"
     style={{ border: 0 }}
