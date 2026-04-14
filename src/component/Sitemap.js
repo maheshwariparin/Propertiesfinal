739694a09d4293bda8657318bb776eae83af3758
parinmaheshwari <125479941+maheshwariparin@users.noreply.github.com>
2026-04-14 20:41:35 +0530

    changes

--- src/component/Sitemap.js
+++ src/component/Sitemap.js
@@ -2,5 +2,5 @@ import Sitemap from 'react-router-sitemap';
 import routes from './routes';
 
 new Sitemap(routes)
-  .build('https://dmhproperties.in')
+  .build('https://dmhproperties.co.in')
   .save('./public/sitemap.xml');
